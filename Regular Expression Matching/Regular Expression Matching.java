502d2524526ac08c3bf86cdb9d0a6bdd2ea263b8
Shuai Yuan <iuanshuai@hotmail.com>
2017-12-10 17:21:15 +0800

    add solution

--- Regular Expression Matching/Regular Expression Matching.java
+++ Regular Expression Matching/Regular Expression Matching.java
@@ -1,21 +1,21 @@
 class Solution {
     public boolean isMatch(String text, String pattern) {
         if (pattern.isEmpty()) {
-			return text.isEmpty();
-		}
-		boolean first_match = (!text.isEmpty() && 
-								(pattern.charAt(0) == text.charAt(0) || pattern.charAt(0) == '.'));
-		if (pattern.length() >= 2 && pattern.charAt(1) == '*') {
-			return (isMatch(text, pattern.substring(2)) ||
-							 (first_match && isMatch(text.substring(1), pattern)));
-		} else {
-			return first_match && isMatch(text.substring(1), pattern.substring(1));
-		}
-	}
-	
-	public static void main(String[] args) {
-		Solution s = new Solution();
-		System.out.println(s.isMatch("abc", ".bc"));
-		System.out.println(s.isMatch("abc", "f*abc"));
-	}
+            return text.isEmpty();
+        }
+        boolean first_match = (!text.isEmpty() && 
+                                (pattern.charAt(0) == text.charAt(0) || pattern.charAt(0) == '.'));
+        if (pattern.length() >= 2 && pattern.charAt(1) == '*') {
+            return (isMatch(text, pattern.substring(2)) ||
+                             (first_match && isMatch(text.substring(1), pattern)));
+        } else {
+            return first_match && isMatch(text.substring(1), pattern.substring(1));
+        }
+    }
+    
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.isMatch("abc", ".bc"));
+        System.out.println(s.isMatch("abc", "f*abc"));
+    }
 }
