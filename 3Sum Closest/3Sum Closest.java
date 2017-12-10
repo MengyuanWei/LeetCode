08d0a79c35e0ff7af3748362d08282bbc804e473
Shuai Yuan <iuanshuai@hotmail.com>
2017-12-10 17:19:07 +0800

    add solution

--- 3Sum Closest/3Sum Closest.java
+++ 3Sum Closest/3Sum Closest.java
@@ -1,38 +1,38 @@
 import java.util.*;
 
 class Solution {
-	public int threeSumClosest(int[] nums, int target) {
-		if (nums.length < 3) {
-			return 0;
-		}
-		Arrays.sort(nums);
-		int sum = nums[0] + nums[1] + nums[2];
-		for (int i = 0; i < nums.length - 2; i++) {
-			int l = i + 1;
-			int r = nums.length - 1;
-			while (l < r) {
-				int t_sum = nums[i] + nums[l] + nums[r];
-				if (t_sum == target) {
-					return t_sum;
-				} else if (sum < target) {
-					if (Math.abs(target - t_sum) < Math.abs(target - sum)) {
-						sum = t_sum;
-					}
-					l++;
-					
-				} else {
-					if (Math.abs(target - t_sum) < Math.abs(target - sum)) {
-						sum = t_sum;
-					}
-					r--;
-				}
-			}
-		}
-		return sum;
-	}
-	public static void main(String[] args) {
-		Solution s = new Solution();
-		int[] test = {-1, 2, 1, -4};
-		System.out.print(s.threeSumClosest(test, 1));
-	}
+    public int threeSumClosest(int[] nums, int target) {
+        if (nums.length < 3) {
+            return 0;
+        }
+        Arrays.sort(nums);
+        int sum = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < nums.length - 2; i++) {
+            int l = i + 1;
+            int r = nums.length - 1;
+            while (l < r) {
+                int t_sum = nums[i] + nums[l] + nums[r];
+                if (t_sum == target) {
+                    return t_sum;
+                } else if (sum < target) {
+                    if (Math.abs(target - t_sum) < Math.abs(target - sum)) {
+                        sum = t_sum;
+                    }
+                    l++;
+                    
+                } else {
+                    if (Math.abs(target - t_sum) < Math.abs(target - sum)) {
+                        sum = t_sum;
+                    }
+                    r--;
+                }
+            }
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] test = {-1, 2, 1, -4};
+        System.out.print(s.threeSumClosest(test, 1));
+    }
 }
--- 3Sum/3Sum.java
+++ 3Sum/3Sum.java
@@ -1,46 +1,46 @@
 import java.util.*;
 
 class Solution {
-	public List<List<Integer>> threeSum(int[] nums) {
-		List<List<Integer>> ans = new ArrayList<>();
-		if (nums.length == 0) {
-			return ans;
-		}
-		Arrays.sort(nums);
-		for (int i = 0; i < nums.length - 2; i++) {
-			if (i > 0 && nums[i - 1] == nums[i]) {
-				continue;
-			}
-			int target = -nums[i];
-			int l = i + 1;
-			int r = nums.length - 1;
-			twoSum(nums, l, r, target, ans);
-		}
-		return ans;
-	}
-	public void twoSum(int[] nums, int l, int r, int target, List<List<Integer>> ans) {
-		while (l < r) {
-			if (nums[l] + nums[r] == target) {
-				ArrayList<Integer> t = new ArrayList<>();
-				t.add(-target);
-				t.add(nums[r]);
-				t.add(nums[l]);
-				ans.add(t);
-				l++;
-				r--;
-				while (l < r && nums[l] == nums[l - 1]) l++;
-				while (l < r && nums[r] == nums[r + 1]) r--;
-			} else if (nums[l] + nums[r] > target) {
-				r--;
-			} else {
-				l++;
-			}
-		}
-	}
-	
-	public static void main(String[] args) {
-		Solution s = new Solution();
-		int[] a = {-1, 0, 1, 2, -1, -4};
-		System.out.print(s.threeSum(a));
-	}
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (nums.length == 0) {
+            return ans;
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i - 1] == nums[i]) {
+                continue;
+            }
+            int target = -nums[i];
+            int l = i + 1;
+            int r = nums.length - 1;
+            twoSum(nums, l, r, target, ans);
+        }
+        return ans;
+    }
+    public void twoSum(int[] nums, int l, int r, int target, List<List<Integer>> ans) {
+        while (l < r) {
+            if (nums[l] + nums[r] == target) {
+                ArrayList<Integer> t = new ArrayList<>();
+                t.add(-target);
+                t.add(nums[r]);
+                t.add(nums[l]);
+                ans.add(t);
+                l++;
+                r--;
+                while (l < r && nums[l] == nums[l - 1]) l++;
+                while (l < r && nums[r] == nums[r + 1]) r--;
+            } else if (nums[l] + nums[r] > target) {
+                r--;
+            } else {
+                l++;
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] a = {-1, 0, 1, 2, -1, -4};
+        System.out.print(s.threeSum(a));
+    }
 }
