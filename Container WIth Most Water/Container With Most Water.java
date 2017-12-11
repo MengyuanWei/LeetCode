304c89b3778970a243106dbd1b46ea9195e15bf2
Shuai Yuan <iuanshuai@hotmail.com>
2017-12-11 16:53:48 +0800

    add solution

--- Container WIth Most Water/Container With Most Water.java
+++ Container WIth Most Water/Container With Most Water.java
@@ -1,23 +1,23 @@
 class Solution {
-	
-	public int maxArea(int[] height) {
-		int maxarea = 0, l = 0, r = height.length - 1;
-		while (l < r) {
-			int h = Math.min(height[l], height[r]);
-			maxarea = Math.max(maxarea, h * (r - l));
-			while (l < r && height[l] <= h) {
-				l++;
-			}
-			while (l < r && height[r] <= h) {
-				r--;
-			}
-		}	
-		return maxarea;
-	}
-	
-	public static void main(String[] args) {
-		Solution s = new Solution();
-		int[]a = new int[]{4, 5, 3, 3, 8};
-		System.out.print(s.maxArea(a));
-	}
+    
+    public int maxArea(int[] height) {
+        int maxarea = 0, l = 0, r = height.length - 1;
+        while (l < r) {
+            int h = Math.min(height[l], height[r]);
+            maxarea = Math.max(maxarea, h * (r - l));
+            while (l < r && height[l] <= h) {
+                l++;
+            }
+            while (l < r && height[r] <= h) {
+                r--;
+            }
+        }	
+        return maxarea;
+    }
+    
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[]a = new int[]{4, 5, 3, 3, 8};
+        System.out.print(s.maxArea(a));
+    }
 }
