9d0c54c09f5070af24645145311d2dd28a739ae5
Shuai Yuan <iuanshuai@hotmail.com>
2017-12-28 14:09:00 +0800

    add solution

--- Combination Sum II/Combination Sum II.java
+++ Combination Sum II/Combination Sum II.java
@@ -1,23 +1,27 @@
 import java.util.*;
 
-class Solution {
+class Untitled {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> list = new ArrayList<>();
-        Array.sort(candidates);
+        Arrays.sort(candidates);
         backtrack(list, new ArrayList<>(), candidates, target, 0);
+        return list;
     }
     private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums, int remain, int start) {
-        if (remain < 0) {
-            return;
-        } else if (remain == 0) {
-            list.add(new ArrayList<>(tempList));
-        } else {
+        if (remain < 0) return;
+        else if (remain == 0) list.add(new ArrayList<>(tempList));
+        else {
             for (int i = start; i < nums.length; i++) {
-                if (i > start && nums[i] == nums[i-1]) continue;
+                if(i > start && nums[i] == nums[i-1]) continue;
                 tempList.add(nums[i]);
                 backtrack(list, tempList, nums, remain - nums[i], i + 1);
                 tempList.remove(tempList.size() - 1);
             }
         }
+    }
+    public static void main(String[] args) {
+        Untitled u = new Untitled();
+        int[] c = {10, 1, 2, 7, 6, 1, 5};
+        System.out.print(u.combinationSum2(c, 8));
     }
 }
