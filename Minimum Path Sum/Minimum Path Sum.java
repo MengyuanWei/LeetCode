b6d61971403c5f3d175ba5ef486e934a714b0736
Shuai Yuan <iuanshuai@hotmail.com>
2018-01-24 14:40:09 +0800

    add solution

--- Minimum Path Sum/Minimum Path Sum.java
+++ Minimum Path Sum/Minimum Path Sum.java
@@ -4,14 +4,12 @@ class Solution {
         int n = grid[0].length;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (i == 0 && j == 0) {
-                    grid[i][j] = grid[i][j];
-                } else if (i == 0 && j != 0) {
-                    grid[i][j] = grid[i][j] + grid[i][j - 1];
+                if (i == 0 && j != 0) {
+                    grid[i][j] += grid[i][j - 1];
                 } else if (i != 0 && j == 0) {
-                    grid[i][j] = grid[i][j] + grid[i - 1][j];
-                } else {
-                    grid[i][j] = Math.min(grid[i][j - 1], grid[i - 1][j]) + grid[i][j];
+                    grid[i][j] += grid[i - 1][j];
+                } else if (i != 0 && j != 0){
+                    grid[i][j] += Math.min(grid[i][j - 1], grid[i - 1][j]);
                 }
             }
         }
