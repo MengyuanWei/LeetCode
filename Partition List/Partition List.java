6c955728b4c1626dc2eb16f19ea8aeda037a85b5
Shuai Yuan <iuanshuai@hotmail.com>
2018-02-21 23:38:01 +0800

    add solution

---
+++ Partition List/Partition List.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode smallerHead = new ListNode(0), biggerHead = new ListNode(0);
+        ListNode s = smallerHead, b = biggerHead;
+        while (head != null) {
+            if (head.val < x) {
+                s.next = head;
+                s = s.next;
+            } else {
+                b.next = head;
+                b = b.next;
+            }
+            head = head.next;
+        }
+        s.next = biggerHead.next; 
+        b.next = null;
+        return smallerHead.next;
+    }
+}
