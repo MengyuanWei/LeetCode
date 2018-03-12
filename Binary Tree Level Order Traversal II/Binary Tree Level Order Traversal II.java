73e37da9a9da52b63a72d1a23b82e4f7a8845d83
Shuai Yuan <iuanshuai@hotmail.com>
2018-03-12 18:11:17 +0800

    add solution

---
+++ Binary Tree Level Order Traversal II/Binary Tree Level Order Traversal II.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> list = new ArrayList<>();
+        helper(list, root, 0);
+        Collections.reverse(list);
+        return list;
+    }
+    public void helper(List<List<Integer>> list, TreeNode root, int height) {
+        if (root == null) return;
+        if (height >= list.size()) {
+            list.add(new ArrayList<>());
+            
+        }
+        list.get(height).add(root.val);
+        helper(list, root.left, height + 1);
+        helper(list, root.right, height + 1);
+    }
+}
