a0cba1c3f1e43b1132fdea0ac6ad515cdb8a1bd7
Shuai Yuan <iuanshuai@hotmail.com>
2018-04-20 23:07:13 +0800

    add solution

--- Same Tree/Same Tree.java
+++ Same Tree/Same Tree.java
@@ -17,6 +17,7 @@ class Solution {
         }
         if (p.val == q.val) {
             return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+            
         }
         return false;
     }
