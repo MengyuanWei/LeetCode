345db7c43d80b2c0feb9b2cfc51e5c3e60464c6d
Shuai Yuan <iuanshuai@hotmail.com>
2018-04-27 15:48:05 +0800

    add solution

---
+++ Maximum Product Subarray/Maximum Product Subarray.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxProduct(int[] A) {
+        if (A.length == 0) {
+            return 0;
+        }
+        
+        int maxherepre = A[0];
+        int minherepre = A[0];
+        int maxsofar = A[0];
+        int maxhere, minhere;
+        
+        for (int i = 1; i < A.length; i++) {
+            maxhere = Math.max(Math.max(maxherepre * A[i], minherepre * A[i]), A[i]);
+            minhere = Math.min(Math.min(maxherepre * A[i], minherepre * A[i]), A[i]);
+            maxsofar = Math.max(maxhere, maxsofar);
+            maxherepre = maxhere;
+            minherepre = minhere;
+        }
+        return maxsofar;
+    }
+    
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] test = {9 , 1, 2, -4, -5};
+        System.out.println(s.maxProduct(test));
+    }
+}
