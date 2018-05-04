b9350e94ef3a3580e21d25ff149f2ab3121c09b5
Shuai Yuan <iuanshuai@hotmail.com>
2018-05-04 23:09:41 +0800

    add solution

--- Best Time to Buy and Sell Stock/Best Time to Buy and Sell Stock.java
+++ Best Time to Buy and Sell Stock/Best Time to Buy and Sell Stock.java
@@ -12,6 +12,7 @@ class Solution {
         }
         return maxPro;
     }
+    
     public static void main(String[] args) {
         int[] test1 = {7, 1, 5, 3, 6, 4};
         int[] test2 = {7, 6, 4, 3, 1};
