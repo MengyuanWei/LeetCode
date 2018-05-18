b117dd3adbd676f717e7a1de70e153f0964c5e89
Shuai Yuan <iuanshuai@hotmail.com>
2018-05-18 14:02:32 +0800

    backtracking

--- Combination Sum II/Combination Sum II.java
+++ Combination Sum II/Combination Sum II.java
@@ -1,27 +1,21 @@
-import java.util.*;
-
-class Untitled {
+class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> list = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(candidates);
-        backtrack(list, new ArrayList<>(), candidates, target, 0);
-        return list;
+        helper(res, new ArrayList<>(), target, candidates, 0);
+        return res;
     }
-    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums, int remain, int start) {
+    public void helper(List<List<Integer>> res, List<Integer> tempList, int remain, int[] candidates, int start) {
         if (remain < 0) return;
-        else if (remain == 0) list.add(new ArrayList<>(tempList));
-        else {
-            for (int i = start; i < nums.length; i++) {
-                if(i > start && nums[i] == nums[i-1]) continue;
-                tempList.add(nums[i]);
-                backtrack(list, tempList, nums, remain - nums[i], i + 1);
+        else if (remain == 0) {
+            res.add(new ArrayList<>(tempList));
+        } else {
+            for (int i = start; i < candidates.length; i++) {
+                if (i > start && candidates[i] == candidates[i - 1]) continue;
+                tempList.add(candidates[i]);
+                helper(res, tempList, remain - candidates[i], candidates, i + 1);
                 tempList.remove(tempList.size() - 1);
             }
         }
-    }
-    public static void main(String[] args) {
-        Untitled u = new Untitled();
-        int[] c = {10, 1, 2, 7, 6, 1, 5};
-        System.out.print(u.combinationSum2(c, 8));
     }
 }
